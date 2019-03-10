61ce963ff003647a5746c2da79a0b72f043b685b
Petr Ivanov <petr.yrs@gmail.com>
2019-03-10 13:11:30 +0300

    Add start position as paramert

--- qrl.php
+++ qrl.php
@@ -70,17 +70,25 @@ if (file_exists('./db.conf')) {
 
 if (isset($help)) {
     $help = "\nVirtuoso QRL tool\nRun: php ";
-    $help .= $argv[0] . " [--qrl_log=file.qrl] [--qf=query_file] [--play] [--mc=5] [--td=0] [--time] [--qn=0] [--rc=1] [--odbc] [--csv=file.csv] [--directly]\n";
+    $help .= $argv[0] . "[--time]\n";
+    $help .= "\nImport params:\n";
+    $help .= "\t [--qrl_log=file.qrl] [--odbc] [--start=0]\n";
+    $help .= "\nExport params: \n";
+    $help .= "\t [--qf=query_file] [--csv=file.csv] [--directly] \n";
+    $help .= "\nReplay params: \n";
+    $help .= "\t [--play] [--mc=5] [--td=0] [--qn=0] [--rc=1] \n";
+    $help .= "\nDescriptions: \n";
     $help .= "--qrl_log \t file name with QRL data \n";
-    $help .= "--qf \t text file with querys. Default querys.dat \n";
-    $help .= "--play \t send querys to server \n";
-    $help .= "--mc \t max concurent connections. Default 5 \n";
-    $help .= "--td \t thread delay in seconds. Default 0 sec \n";
-    $help .= "--time \t calculate duration time \n";
-    $help .= "--qn \t query number \n";
-    $help .= "--rc \t count of repeate query \n";
-    $help .= "--odbc \t use ODBC for connection. Params in odbc.conf \n";
-    $help .= "--csv \t export querys to CSV file \n";
+    $help .= "--qf \t\t text file with querys. Default querys.dat \n";
+    $help .= "--play \t\t send querys to server \n";
+    $help .= "--mc \t\t max concurent connections. Default 5 \n";
+    $help .= "--td \t\t thread delay in seconds. Default 0 sec \n";
+    $help .= "--time \t\t calculate duration time \n";
+    $help .= "--qn \t\t query number \n";
+    $help .= "--rc \t\t count of repeate query \n";
+    $help .= "--odbc \t\t use ODBC for connection. Params in odbc.conf \n";
+    $help .= "--start \t\t start position in qrl log \n";
+    $help .= "--csv \t\t export querys to CSV file \n";
     $help .= "--directly \t directly write imported data \n";
     echo $help . "\n";
     exit(0);
@@ -100,6 +108,8 @@ if (isset($odbc)) {
     }
 }
 
+$start = $start ?? 0;
+
 // read from QRL log file
 if (isset($qrl_log) && !isset($directly)) {
     $outFile = $qf ?? 'querys.dat';
@@ -108,7 +118,7 @@ if (isset($qrl_log) && !isset($directly)) {
     } else {
         $importer = new qrltool\qrlImporter($qrl_log, $db);
     }
-    $data = $importer->getData();
+    $data = $importer->getData(100, $start);
 
     $f = fopen($outFile, 'wb');
     foreach ($data as $item) {
@@ -145,7 +155,7 @@ if (isset($directly) && isset($qrl_log) && isset($csv)) {
     }
     $totalCount = $importer->getCount();
 
-    $i = 0;
+    $i = $start;
     while ($i < $totalCount) {
         echo date('H:i:s',time()). " try $i of $totalCount ";
         $data = $importer->getPart($countPeer, $i);
